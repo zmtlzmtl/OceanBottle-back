9b9e2a2326f12b569167415546b4e03f080a3fc7
kwakseungmin23 <lookingforfandl@gmail.com>
2023-02-25 21:22:57 +0900

    fix: routes index fix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // 예시 4~6번째 줄
-const UserRouter = require("./users.routes");
+const UserRouter = require("./users");
 const plusscriptRouter = require("./plusScript.routes");
 
 router.use("/", [UserRouter, plusscriptRouter]);
