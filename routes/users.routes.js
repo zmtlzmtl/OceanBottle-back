33b017c2e7c777d7597daee1e9f839c66ed5ed93
DONGYUN SHIN <windsdy1@gmail.com>
2023-02-25 14:28:51 +0900

    feat: 유저라우터

---
+++ routes/users.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// const UsersController = require('../controllers/users.controller');  //[class UsersController]
+// const usersController = new UsersController();
+
+// router.post('/user', usersController.postCreateUser);
+// router.post('/login', usersController.postLoginUser);
+
+module.exports = router;
