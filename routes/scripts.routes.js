4f34417154870d7941feec1a99c4dcfde0220b32
Leeseungwoon <seong3546@gmail.com>
2023-02-27 22:15:39 +0900

    scriptsRoutes 재구현 완료

--- routes/scripts.routes.js
+++ routes/scripts.routes.js
@@ -7,11 +7,16 @@ const scriptsController = new ScriptsController();
 
 //first script 생성하기
 router.post('/',authMiddleware, scriptsController.createScript);
-//script 상세보여주기
-router.get('/:scripId/', scriptsController.findOneScript);
-//script 랜덤 5개 보여주기
-router.get('/', scriptsController.findRandomScript);
 //script 전체 조회
-router.get('/all/',scriptsController.findAllScript);
+router.get('/all',scriptsController.getAllController);
+//script 상세 조회
+router.get('/:scriptId/', scriptsController.getDetailController);
+//script 수정
+router.patch('/:scriptId/', scriptsController.updateController);
+//script 삭제
+router.delete('/:scriptId/', scriptsController.deleteController);
+//script 랜덤 5개 보여주기
+router.get('/', scriptsController.getRandomController);
+
 
 module.exports = router;
--- services/scripts.service.js
+++ services/scripts.service.js
@@ -1,68 +1,50 @@
 const ScriptsRepository = require('../repositories/scripts.repository')
 const PlusScriptRepository = require('../repositories/plusscript.repository')
 
+
 class ScriptsService{
     constructor(){
         this.scriptsRepository = new ScriptsRepository();
         this.plusScriptRepository = new PlusScriptRepository(); 
     }
     
-    //Script 생성
+    //create
     createScript = async({userId,genre, title, content}) =>{       
         await this.scriptsRepository.createScript({userId, genre, title, content})
         
         return;
     }
 
-    //Script 상세 조회
-    findOneScript = async( scriptId ) => {
-        const script = await this.ScriptsRepository.findOneScript( scriptId )
-        const plusScript  = await this.plusScriptRepository.findPlusScripts( scriptId );
-        //사용자에게 보여줄 데이터 가공 
-        const result = [
-            {
-            scriptId: script.scriptId,
-            userId: script.UserId,
-            id:script.user.id,
-            genre: script.genre,
-            title: script.title,
-            content: script.content,
-            createAt:script.createAt,
-            updatedAt:script.updatedAt
-            },
-            {
-            plusScriptId: plusScript.plusScriptId,
-            userId: plusScript.UserId,
-            id:plusScript.id,
-            title: plusScript.title,
-            content:plusScript.content,
-            createAt:plusScript.createAt,
-            updatedAt:plusScript.updatedAt
-            }
-    ];   
-   
-        return result;
+    //getAll
+    getAllService = async() => {
+        const scripts = await this.scriptsRepository.getAllRepo();
+
+        return scripts;
     }
 
-    //Script 랜덤 5개 가져오기
-    findRandomscript = async()=> {
-        const randomScript = await this.ScriptsRepository.findRandomscript()
-        const randomData = [];
-        for(let i = 0; i < 5; i++){
-            randomData = randomScript[Math.floor(Math.random() * randomScript.length)];
-       }
-       return randomData;       
+    //getDetail
+    getDetailService = async({ scriptId }) => {
+        const script = await this.scriptsRepository.getDetailRepo({ scriptId })
+        return script;
     }
 
-    findAllscript = async() => {
-        const allScript = await this.ScriptsRepository.findAllScript()
+    //update
+    updateService = async({ scriptId, genre, title, content }) => {
+        await this.scriptsRepository.updateRepo({ scriptId, genre, title, content });
+        return;
+    }
 
-        return allScript;
-    }    
+    //delete
+    deleteService = async({ scriptId }) => {
+        await this.scriptsRepository.deleteRepo({ scriptId });
+        return;
+    }
 
-    
-
-
+    //getRandom
+    getRandomService = async () => {
+        const randomScripts = await this.scriptsRepository.getRandomRepo();
+        return randomScripts;
+    }
 }
 
 module.exports = ScriptsService;
