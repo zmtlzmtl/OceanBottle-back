29573e700939ca92f3e5362adea9f41359b806c4
DONGYUN SHIN <windsdy1@gmail.com>
2023-02-27 10:45:10 +0900

    fix: user경로 변경

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // router.use("/posts", [postsRouter]);
 
 const ScriptRouter = require('./scripts.routes')
-const UserRouter = require("./users");
+const UserRouter = require("./users.routes");
 const plusscriptRouter = require("./plusScript.routes");
 
 router.use("/scripts", [ScriptRouter]);
