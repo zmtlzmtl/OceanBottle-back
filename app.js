dc49f2000a9be115a2c8f74262287e301099fd5d
DONGYUN SHIN <windsdy1@gmail.com>
2023-02-27 21:35:00 +0900

    feat: credentials, cors 주소 제한

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ const routes = require('./routes');
 const cors = require('cors');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 
+let corsOptions = {
+  origin: 'https://www.localhost:3000',
+  credentials: true
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use('/', [routes]);
 app.use(errorMiddleware);
